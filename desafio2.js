12b8fbe37f3bb4fa686ef37a49f9f36e280761c5
joao alexandre <joaoalexandre@gmail.com>
2024-07-01 15:55:06 -0300

    segundo desafio

---
+++ desafio2.js
@@ -0,0 +1,10 @@
+//Declaração de variáveis
+let vitorias ="50"
+let derrotas = "2"
+
+//Variável para armazenar o saldo de vitorias do jogador
+let saldoVitorias =""
+
+saldoVitorias = vitorias - derrotas
+
+console.log (saldoVitorias)
